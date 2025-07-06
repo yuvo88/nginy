84b3cae127b37e46b7339982ba6b918e45cf4770
Yuval Schnitzer <yuvals@tamnoon.io>
2025-07-06 16:50:43 -0400

    parsing get request successfully

--- src/server.c
+++ src/server.c
@@ -14,7 +14,12 @@
 #define SERVER_RESPONSE                                                        \
   "HTTP/1.0 200 OK\nServer: nginy\nContent-type: text/html; "                  \
   "charset=utf-8\nContent-Length: 0\n"
+#define MAX_METHOD_LEN 10
+#define RECEIEVE_BUFFER_LENGTH 1024
 
+HttpRequestLine parseRequestLine(char *requestLineString);
+HttpRequest parseHttpRequest(char *receiveBuffer);
+HttpHeader parseHttpHeader(char *httpHeaderString);
 int createServer(const char *bindIp, int bindPort) {
   int serverFileDescriptor, connectionFileDescriptor;
   struct sockaddr_in bindAddress;
@@ -37,24 +42,71 @@ int createServer(const char *bindIp, int bindPort) {
 void handleConnection(int serverFileDescriptor) {
   socklen_t addressSize;
   struct sockaddr_in remoteAddress;
-  char receiveBuffer[1024] = {};
+  char receiveBuffer[RECEIEVE_BUFFER_LENGTH] = {};
 
   addressSize = sizeof(remoteAddress);
   int connectionFileDescriptor = accept(
       serverFileDescriptor, (struct sockaddr *)&remoteAddress, &addressSize);
   assert(connectionFileDescriptor != -1);
 
-  recv(connectionFileDescriptor, receiveBuffer, 1024, 0);
-  char *splitBuffer = strndup(receiveBuffer, 1024);
-  char *toSend = splitBuffer;
-  printf("%s\n", splitBuffer);
-  char *token = strsep(&toSend, "\n");
-  printf("%s\n", token);
+  recv(connectionFileDescriptor, receiveBuffer, RECEIEVE_BUFFER_LENGTH, 0);
+  HttpRequest parsedRequest = parseHttpRequest(receiveBuffer);
+  printf("method: %d, uri: %s\n", parsedRequest.requestLine.method,
+         parsedRequest.requestLine.uri);
+  for (int i = 0; i < parsedRequest.headers.length; i++) {
+    printf("key: %s, value: %s\n", parsedRequest.headers.headers[i].headerKey,
+           parsedRequest.headers.headers[i].headerValue);
+  }
   send(connectionFileDescriptor, SERVER_RESPONSE, strlen(SERVER_RESPONSE), 0);
   closeFileDescriptor(connectionFileDescriptor);
-  free(splitBuffer);
 }
 
 void closeFileDescriptor(int fileDescriptor) {
   assert(close(fileDescriptor) != -1);
+}
+
+HttpRequest parseHttpRequest(char *receiveBuffer) {
+  HttpRequest httpRequest;
+  char *splitBuffer = strndup(receiveBuffer, RECEIEVE_BUFFER_LENGTH);
+  char *runner = splitBuffer;
+  char *header;
+  char *requestLineString = strsep(&runner, "\n");
+  httpRequest.requestLine = parseRequestLine(requestLineString);
+  header = strsep(&runner, "\n");
+  for (int i = 0; header[0] != '\r' && i < HEADER_ARRAY_LENGTH; i++) {
+    HttpHeader httpHeader = parseHttpHeader(header);
+    httpRequest.headers.headers[i] = httpHeader;
+    httpRequest.headers.length = i + 1;
+    header = strsep(&runner, "\n");
+  }
+  free(splitBuffer);
+  return httpRequest;
+}
+
+HttpRequestLine parseRequestLine(char *requestLineString) {
+  HttpRequestLine httpRequestLine;
+  char *splitBuffer = strndup(requestLineString, RECEIEVE_BUFFER_LENGTH);
+  char *runner = splitBuffer;
+  char *method = strsep(&runner, " ");
+  if (strncmp(method, "GET", MAX_METHOD_LEN) != 0) {
+    httpRequestLine.method = UNKNOWN;
+  } else {
+    httpRequestLine.method = GET;
+  }
+  char *uri = strsep(&runner, " ");
+  strncpy(httpRequestLine.uri, uri, URI_LENGTH);
+
+  free(splitBuffer);
+  return httpRequestLine;
+}
+
+HttpHeader parseHttpHeader(char *httpHeaderString) {
+  HttpHeader httpHeader;
+  char *splitBuffer = strndup(httpHeaderString, RECEIEVE_BUFFER_LENGTH);
+  char *runner = splitBuffer;
+  char *headerKey = strsep(&runner, ":");
+  strncpy(httpHeader.headerKey, headerKey, HEADER_KEY_SIZE);
+  char *headerValue = strsep(&runner, ":");
+  strncpy(httpHeader.headerValue, headerValue + 1, HEADER_VALUE_SIZE);
+  return httpHeader;
 }
--- src/server.h
+++ src/server.h
@@ -1,6 +1,29 @@
 #ifndef __server_h
 #define __server_h
-void handleConnection(int serverFileDescriptor);
-int createServer(const char *bindIp, int bindPort);
-void closeFileDescriptor(int fileDescriptor);
+#include <stdint.h>
+#define HEADER_KEY_SIZE 32
+#define HEADER_VALUE_SIZE 256
+#define HEADER_ARRAY_LENGTH 20
+#define URI_LENGTH 256
+typedef enum Method { GET, UNKNOWN } Method;
+typedef struct HttpHeader {
+    char headerKey[HEADER_KEY_SIZE];
+    char headerValue[HEADER_VALUE_SIZE];
+} HttpHeader;
+typedef struct HttpHeaders {
+    uint8_t length;
+    HttpHeader headers[HEADER_ARRAY_LENGTH];
+} HttpHeaders;
+
+typedef struct HttpRequestLine {
+    Method method;
+    char uri[URI_LENGTH];
+} HttpRequestLine;
+typedef struct HttpRequest {
+    HttpHeaders headers;
+    HttpRequestLine requestLine;
+} HttpRequest;
+void handleConnection (int serverFileDescriptor);
+int createServer (const char* bindIp, int bindPort);
+void closeFileDescriptor (int fileDescriptor);
 #endif
